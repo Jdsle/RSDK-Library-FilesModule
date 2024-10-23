df1adc75be3003da65770b712a139df4ec33ba89
Jd-1206 <Jd1.ext4@gmail.com>
2024-10-23 14:27:53 -0400

    add files module

---
+++ FilesModule/README.md
@@ -0,0 +1,10 @@
+## Files Module
+
+description about it
+
+Building
+```
+emcmake cmake -B build
+cmake -B build
+
+```
---
+++ FilesModule/build/CMakeFiles/FilesApp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FilesApp.dir/Files.c.o"
+  "CMakeFiles/FilesApp.dir/Files.c.o.d"
+  "Files.pdb"
+  "Files.wasm"
+  "Files.wasm.manifest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/FilesApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
